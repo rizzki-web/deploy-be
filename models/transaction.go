12b5da8ca1fa72c59bc8bf077b58e9f01b9c0120
rizzki-web <muahmad.rizki@gmail.com>
2022-08-29 14:06:29 +0700

    upate

--- models/transaction.go
+++ models/transaction.go
@@ -3,15 +3,16 @@ package models
 import "time"
 
 type Transaction struct {
-	ID      int                  `json:"id" gorm:"primary_key:auto_increment"`
-	BuyerID int                  `json:"buyer_id"`
-	Buyer   UsersProfileResponse `json:"buyer"`
-	Price   int                  `json:"price"`
-	Product Product               `json:"product_id"`
-	Total     int       `json:"total"`
-	Status    string    `json:"status"  gorm:"type:varchar(25)"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"-"`
+	ID        int                  `json:"id" gorm:"primary_key:auto_increment"`
+	BuyerID   int                  `json:"buyer_id"`
+	Buyer     UsersProfileResponse `json:"buyer"`
+	Price     int                  `json:"price"`
+	Product   Product              `json:"product"`
+	ProductID int 					`json:"product_id"`
+	Total     int                  `json:"total"`
+	Status    string               `json:"status"  gorm:"type:varchar(25)"`
+	CreatedAt time.Time            `json:"-"`
+	UpdatedAt time.Time            `json:"-"`
 }
 
 type TransactionResponse struct {
