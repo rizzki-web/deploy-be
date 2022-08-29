aafaf2d79cebbdbb30380c507a567a791d886fd8
rizzki-web <muahmad.rizki@gmail.com>
2022-08-29 15:52:18 +0700

    update

--- routes/product.go
+++ routes/product.go
@@ -3,7 +3,6 @@ package routes
 import (
 	"waysbuck/handlers"
 	mysql "waysbuck/pkg"
-	"waysbuck/pkg/middleware"
 	"waysbuck/repositories"
 
 	"github.com/gorilla/mux"
@@ -14,11 +13,11 @@ func ProductRoutes(r *mux.Router) {
 	h := handlers.HandlerProduct(productRepository)
 
 	// r.HandleFunc("/products", h.FindProducts).Methods("GET")
-	r.HandleFunc("/products", middleware.Auth(h.FindProducts)).Methods("GET")
+	r.HandleFunc("/products",h.FindProducts).Methods("GET")
 
 	r.HandleFunc("/product/{id}", h.GetProduct).Methods("GET")
 
-	r.HandleFunc("/product", middleware.Auth(h.CreateProduct)).Methods("POST")
+	r.HandleFunc("/product", h.CreateProduct).Methods("POST")
 	//r.HandleFunc("/product", h.CreateProduct).Methods("POST")
 
 	r.HandleFunc("/product/{id}", h.UpdateProduct).Methods("PATCH")
